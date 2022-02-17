20dfa29c41219a73b5ecb7bd8f6180496925e3a7
laulujan <laura.lujan@wizeline.com>
2022-02-16 19:18:15 -0600

    styles: update styles for product detail

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,24 +1,53 @@
 import React from 'react';
+import {
+  Title,
+  Label,
+  Tag,
+  Button,
+  Text,
+  ButtonAdd,
+  Box,
+} from './Details.styled';
 
 const Details = () => {
   return (
     <div>
-      <div>PRODUCT TITLE</div>
-      <div>PRICE</div>
-      <div>SKU</div>
-      <div>CATEGORY</div>
-      <div>
-        TAGS
-        <div>tag</div>
-        <div>tag</div>
-        <div>tag</div>
-      </div>
-      <div>-</div>
-      <div>quatity</div>
-      <div>+</div>
-      <button>add to cart</button>
-      <div>Description</div>
-      <div>SPECS</div>
+      <Title>PRODUCT TITLE</Title>
+      <Label>
+        <b>Price</b> $1234
+      </Label>
+      <Label>
+        <b>SKU:</b> #IAUEIRJKDSFJALS
+      </Label>
+      <Label>
+        <b>Category:</b> Decoration
+      </Label>
+      <Label>
+        <b>Tags:</b>
+        <Tag>Lighting</Tag>
+        <Tag>Modern</Tag>
+        <Tag>Cool</Tag>
+      </Label>
+      <Label>
+        <b>Quantity:</b>
+      </Label>
+      <Box>
+        <Button>-</Button>
+        <Label> 1 </Label>
+        <Button>+</Button>
+      </Box>
+
+      <ButtonAdd>add to cart</ButtonAdd>
+      <Label>
+        <b>Description:</b>
+        <Text>some large text that describes the product</Text>
+      </Label>
+      <Label>
+        <b>Specs:</b>
+        <Text>a list of specs</Text>
+        <Text>for the product </Text>
+        <Text>To display on scren</Text>
+      </Label>
     </div>
   );
 };
--- src/pages/ProductDetail/ProductDetail.styled.js
+++ src/pages/ProductDetail/ProductDetail.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 90%;
-  margin: auto;
+  margin: 20px auto;
   display: flex;
   flex-wrap: wrap;
   min-height: 100vh;
